3d760008c8e71c9c8e301c21e009adc3af31eda6
Lucas Frias <lafrias2@gmail.com>
2020-03-17 23:15:01 -0700

    add api call

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+var express    = require('express');      
+var app        = express();                 
+var bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Successfully connected to our server!</h1>');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.send("Welcome to CS 4800 Server!")
+  });
+
+//set port
+var port = process.env.PORT || 8080;
+
+var router = express.Router();             
+
+// middleware
+router.use(function(req, res, next) {
+  // need authentication for requests
+  console.log('Middleware!');
+  next();
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+//GET request using url, http://localhost:8080/api/message
+router.route('/message')
+    .get(function(req, res) {
+        res.json({ newMessage: 'Your message has been recieved by server! ' +
+        'Message sent = ' + req.body.message,
+      statusCode: 200});
+        
+    });
 
-//test
+//register routes
+app.use('/api', router);
+
+//start server
+app.listen(port);
+console.log('Running on port ' + port);
