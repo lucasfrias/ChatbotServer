3df42fde7a84c67f548aa98e9dee7cb33a434a59
Lucas Frias <lafrias2@gmail.com>
2020-03-17 23:12:44 -0700

    Added api call

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Successfully connected to our server!</h1>');
 });
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
 });
+
+//test
