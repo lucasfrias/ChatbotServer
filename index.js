ab9f743563a896c30f13aad7ec3e4f8520c16eb8
Lucas Frias <lafrias2@gmail.com>
2020-03-26 19:54:45 -0700

    Added natural language proccessing.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express    = require('express');      
 var app        = express();                 
 var bodyParser = require('body-parser');
+var natural    = require('natural');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -26,9 +27,35 @@ router.route('/message')
     .get(function(req, res) {
         res.json({ newMessage: 'Your message has been recieved by server! ' +
         'Message sent = ' + req.body.message,
-      statusCode: 200});
-        
+      statusCode: 200,
+      tokenize: nlpTokenizing(req.body.message),
+      stems: nlpStemming(req.body.message)});
     });
+
+
+
+/*
+Natural Language Processing (NLP) is a subfield that studies the interaction between computers and 
+humans (natural) languages. It can process and analyze large amounts of natural language data. 
+Tokenization is a process in which it parses input and classifies sections of a string of input characters.
+This function returns an array of each word in the sentence. 
+*/
+function nlpTokenizing(message){
+  var tokenizer = new natural.WordTokenizer();
+  console.log(tokenizer.tokenize(message));
+  return tokenizer.tokenize(message);
+}
+
+/*
+Stemming reduces words to their stem/root word. It filters and removes 'stop words', which are 
+common connecting words. Examples include 'the', 'at', 'is', 'which', and more. 
+This function returns an array of each word remaining after stemming filtering.
+*/
+function nlpStemming(message){
+  natural.PorterStemmer.attach();
+  console.log(message.tokenizeAndStem());
+  return message.tokenizeAndStem();
+}
 
 //register routes
 app.use('/api', router);
