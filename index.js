c53b278266f69d7b5d36cd0a8288ebd103cf1ab9
Lucas Frias <lafrias2@gmail.com>
2020-04-09 20:02:20 -0700

    added database call

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var express    = require('express');
 var app        = express();                 
 var bodyParser = require('body-parser');
 var natural    = require('natural');
+var Request    = require("request");
+const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-    res.send("Welcome to CS 4800 Server!")
+    res.json({
+      message: "Welcome to CS 4800 Server! You have successfully connected!"
+      });
   });
 
 //set port
@@ -19,20 +23,66 @@ var router = express.Router();
 router.use(function(req, res, next) {
   // need authentication for requests
   console.log('Middleware!');
+  //const error = new Error("Not found");
+ //error.status = 404;
   next();
 });
 
-//GET request using url, http://localhost:8080/api/message
-router.route('/message')
-    .get(function(req, res) {
-        res.json({ newMessage: 'Your message has been recieved by server! ' +
-        'Message sent = ' + req.body.message,
-      statusCode: 200,
-      tokenize: nlpTokenizing(req.body.message),
-      stems: nlpStemming(req.body.message)});
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error',
+    },
+  });
+});
+
+/*
+GET request using url, http://localhost:8080/api/message
+Heroku app url, https://chatbot-server4800.herokuapp.com/api/message
+*/
+router.post('/message', function(req, res) {
+      callDatabase(req.body.message, function(result){
+        var parsedJson = JSON.parse(result);
+        if(parsedJson.status.code != 200){
+          res.json({
+            message: "There is an error from the database side!",
+            code: parsedJson.status.code,
+            errorType: parsedJson.status.errorType,
+            errorDetails: parsedJson.status.errorDetails
+          });
+        }
+        else{res.json({ 
+          newMessage: parsedJson.result.fulfillment.speech,
+          statusCode: 200
+        });
+      }
     });
+  });
 
-
+  /*
+  This method makes an api call to dialog flow created by the database team. A sessionID is a UUID uniquely identified for every request.
+  Authorization header is required for request.
+  */
+function callDatabase(message, callback){ 
+  Request.post({
+    "headers": { 
+      "content-type": "application/json",
+      "Authorization": "Bearer aced3eb074ee44b0956904ec93a46507"},
+    "url": "https://api.dialogflow.com/v1/query?v=20150910",
+    "body": JSON.stringify({
+      "query": message,
+      "sessionId": uuidv4(),
+      "lang":"en"
+    })
+  }, (error, response, body) => {
+    if(error) {
+      const error = new Error("Error from database!");
+      next(error);
+    }
+    callback(body);
+  });
+}
 
 /*
 Natural Language Processing (NLP) is a subfield that studies the interaction between computers and 
@@ -42,7 +92,7 @@ This function returns an array of each word in the sentence.
 */
 function nlpTokenizing(message){
   var tokenizer = new natural.WordTokenizer();
-  console.log(tokenizer.tokenize(message));
+  //console.log(tokenizer.tokenize(message));
   return tokenizer.tokenize(message);
 }
 
@@ -53,7 +103,7 @@ This function returns an array of each word remaining after stemming filtering.
 */
 function nlpStemming(message){
   natural.PorterStemmer.attach();
-  console.log(message.tokenizeAndStem());
+  //console.log(message.tokenizeAndStem());
   return message.tokenizeAndStem();
 }
 
