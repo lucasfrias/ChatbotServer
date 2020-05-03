134d5d2dc00d020e85b906247fcae04eec109eae
Lucas Frias <lafrias2@gmail.com>
2020-05-03 02:18:50 -0700

    access control allow origin header

--- index.js
+++ index.js
@@ -10,15 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const corsOptions = {
-  origin: true,
-  credentials: true
-}
-app.options('*', cors(corsOptions));
-
-app.use((req, res, next) => {
+app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
