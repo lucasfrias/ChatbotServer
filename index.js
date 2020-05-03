1657905f6c36fd5e96bd43732cf7498f9f9fb5a6
Lucas Frias <lafrias2@gmail.com>
2020-05-03 01:57:27 -0700

    cors change

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
+app.options('*', cors(corsOptions));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
